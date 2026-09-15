0c6fde812472094ac582e80de2af1377c3f0bfa5
Mohamed Mohsen <Mohamedsn101@gmail.com>
2026-09-15 04:48:28 +0300

    Edit 3 Bugs

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetLocale
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $supportedLocales = array_keys(config('app.supported_locales', []));
+        $fallbackLocale = config('app.fallback_locale', config('app.locale'));
+        $locale = $request->session()->get('locale', config('app.locale'));
+
+        if (! in_array($locale, $supportedLocales, true)) {
+            $locale = $fallbackLocale;
+            $request->session()->put('locale', $locale);
+        }
+
+        App::setLocale($locale);
+
+        return $next($request);
+    }
+}
